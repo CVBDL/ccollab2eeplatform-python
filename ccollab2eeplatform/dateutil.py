424d86181173bca41fc276c1aecfd94104ea47e8
zhongsp <patrick.zhongsp@gmail.com>
2017-03-27 17:31:22 +0800

    refactor

--- ccollab2eeplatform/dateutil.py
+++ ccollab2eeplatform/dateutil.py
@@ -2,6 +2,16 @@ from datetime import date
 
 
 def to_isoformat(date_str):
+    """Convert an ISO 8601 like date string to standard ISO 8601 format.
+
+    Args:
+        date_str (str): An ISO 8601 like date string.
+    Returns:
+        str: A standard ISO 8601 date string.
+    Examples:
+        >>> to_isoformat('2017-1-1')
+        2017-01-01
+    """
     message = 'Error occurred parsing date to ISO format.'
 
     if not isinstance(date_str, str):
