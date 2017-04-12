eaaf3bf0c19f244a90dbf74bf1b3c982f706a40f
zhongsp <patrick.zhongsp@gmail.com>
2017-04-12 17:03:11 +0800

    refactor

--- ccollab2eeplatform/manager.py
+++ ccollab2eeplatform/manager.py
@@ -56,7 +56,8 @@ class RecordManager:
                                        UsersSettings.list_login_names())
         return creator_filter.filter()
 
-    def _save_to_eeplatform(self, setting, schema, data):
+    def _save(self, setting, schema, data):
+        """Save data table to EagleEye platform."""
         chart_id = setting['_id']
         data_table = gviz_api.DataTable(schema, data=data)
         data_table_json_obj = json.loads(data_table.ToJSon().decode('utf-8'))
@@ -66,33 +67,35 @@ class RecordManager:
         print(data_table_json_obj)
         print()
 
-    def _process_count_by_month_from_product(self, product, setting):
-        """Record count by month.
-
-        Data table:
-        Month    Count
-        2016-01  20
-        2016-03  10
-        2016-05  25
-        """
-        schema = [('Month', 'string'), ('Count', 'number')]
-        data = []
-        valid_records = self._get_valid_records()
-        product_records = ProductFilter(valid_records, product).filter()
-        stat = RecordsStatistics(product_records)
-        count_by_month = {}
-        for month, group in stat.groupby_review_creation_month:
-            count_by_month[month] = RecordsStatistics(list(group)).count
-        month_range = utils.month_range(self._start_date, self._end_date)
-        for month in month_range:
-            data.append([month, count_by_month.get(month, 0)])
-        self._save_to_eeplatform(setting, schema, data)
-
     def process_count_by_month_from_product(self):
         """Count by month from product."""
         name = 'count_by_month_from_product'
+
+        def _process_count_by_month_from_product(product, setting):
+            """Record count by month.
+
+            Data table:
+            Month    Count
+            2016-01  20
+            2016-03  10
+            2016-05  25
+            """
+            schema = [('Month', 'string'), ('Count', 'number')]
+            data = []
+            valid_records = self._get_valid_records()
+            product_records = ProductFilter(valid_records, product).filter()
+            stat = RecordsStatistics(product_records)
+            count_by_month = {}
+            for month, group in stat.groupby_review_creation_month:
+                count_by_month[month] = RecordsStatistics(list(group)).count
+            month_range = utils.month_range(self._start_date, self._end_date)
+            for month in month_range:
+                data.append([month, count_by_month.get(month, 0)])
+            self._save(setting, schema, data)
+
+        # Start to process for all products.
         for product, setting in self._settings[name].items():
-            self._process_count_by_month_from_product(product, setting)
+            _process_count_by_month_from_product(product, setting)
 
     def process_count_by_product(self):
         """Record count by product.
@@ -111,7 +114,7 @@ class RecordManager:
         for product, group in stat.groupby_creator_product_name:
             _stat = RecordsStatistics(list(group))
             data.append([product, _stat.count])
-        self._save_to_eeplatform(setting, schema, data)
+        self._save(setting, schema, data)
 
     def process_comment_density_uploaded_by_product(self):
         """Comment density(uploaded) by product.
@@ -130,7 +133,7 @@ class RecordManager:
         for product, group in stat.groupby_creator_product_name:
             _stat = RecordsStatistics(list(group))
             data.append([product, _stat.comment_density_uploaded])
-        self._save_to_eeplatform(setting, schema, data)
+        self._save(setting, schema, data)
 
     def process_comment_density_changed_by_product(self):
         """Comment density(changed) by product.
@@ -149,36 +152,38 @@ class RecordManager:
         for product, group in stat.groupby_creator_product_name:
             _stat = RecordsStatistics(list(group))
             data.append([product, _stat.comment_density_changed])
-        self._save_to_eeplatform(setting, schema, data)
-
-    def _process_comment_density_changed_by_month_from_product(
-            self, product, setting):
-        """Comment density(changed) by month.
-
-        Data table:
-        Month    Comments/KLOCC
-        2016-01  0.1
-        2016-02  0.034
-        """
-        schema = [('Month', 'string'), ('Comments/KLOCC', 'number')]
-        data = []
-        valid_records = self._get_valid_records()
-        product_records = ProductFilter(valid_records, product).filter()
-        stat = RecordsStatistics(product_records)
-        density_by_month = {}
-        for month, group in stat.groupby_review_creation_month:
-            _stat = RecordsStatistics(list(group))
-            density_by_month[month] = _stat.comment_density_changed
-        month_range = utils.month_range(self._start_date, self._end_date)
-        for month in month_range:
-            data.append([month, density_by_month.get(month, 0)])
-        self._save_to_eeplatform(setting, schema, data)
+        self._save(setting, schema, data)
 
     def process_comment_density_changed_by_month_from_product(self):
         """Generate chart of comment density(changed) by month from product."""
         name = 'comment_density_changed_by_month_from_product'
+
+        def _process_comment_density_changed_by_month_from_product(
+                product, setting):
+            """Comment density(changed) by month.
+
+            Data table:
+            Month    Comments/KLOCC
+            2016-01  0.1
+            2016-02  0.034
+            """
+            schema = [('Month', 'string'), ('Comments/KLOCC', 'number')]
+            data = []
+            valid_records = self._get_valid_records()
+            product_records = ProductFilter(valid_records, product).filter()
+            stat = RecordsStatistics(product_records)
+            density_by_month = {}
+            for month, group in stat.groupby_review_creation_month:
+                _stat = RecordsStatistics(list(group))
+                density_by_month[month] = _stat.comment_density_changed
+            month_range = utils.month_range(self._start_date, self._end_date)
+            for month in month_range:
+                data.append([month, density_by_month.get(month, 0)])
+            self._save(setting, schema, data)
+
+        # Start to process for all products.
         for product, setting in self._settings[name].items():
-            self._process_comment_density_changed_by_month_from_product(
+            _process_comment_density_changed_by_month_from_product(
                 product, setting)
 
     def process_defect_density_uploaded_by_product(self):
@@ -198,7 +203,7 @@ class RecordManager:
         for product, group in stat.groupby_creator_product_name:
             _stat = RecordsStatistics(list(group))
             data.append([product, _stat.defect_density_uploaded])
-        self._save_to_eeplatform(setting, schema, data)
+        self._save(setting, schema, data)
 
     def process_defect_density_changed_by_product(self):
         """Defect density(changed) by product.
@@ -220,38 +225,39 @@ class RecordManager:
         for product, group in stat.groupby_creator_product_name:
             _stat = RecordsStatistics(list(group))
             data.append([product, _stat.defect_density_changed])
-        self._save_to_eeplatform(setting, schema, data)
-
-
-    def _process_inspection_rate_by_month_from_product(self, product, setting):
-        """Inspection rate by month.
-
-        Data table:
-        Month  KLOCC/Hour
-        2016-01  0.1
-        2016-03  0.2
-        2016-05  0.3
-
-        Returns:
-            A tuple of column definition and data.
-        """
-        schema = [('Month', 'string'), ('KLOCC/Hour', 'number')]
-        data = []
-        valid_records = self._get_valid_records()
-        product_records = ProductFilter(valid_records, product).filter()
-        stat = RecordsStatistics(product_records)
-        stat_by_month = {}
-        for month, group in stat.groupby_review_creation_month:
-            _stat = RecordsStatistics(list(group))
-            stat_by_month[month] = _stat.inspection_rate
-        month_range = utils.month_range(self._start_date, self._end_date)
-        for month in month_range:
-            data.append([month, stat_by_month.get(month, 0)])
-        self._save_to_eeplatform(setting, schema, data)
+        self._save(setting, schema, data)
 
     def process_inspection_rate_by_month_from_product(self):
         """Inspection rate by month from product."""
         name = 'inspection_rate_by_month_from_product'
+
+        def _process_inspection_rate_by_month_from_product(product, setting):
+            """Inspection rate by month.
+
+            Data table:
+            Month  KLOCC/Hour
+            2016-01  0.1
+            2016-03  0.2
+            2016-05  0.3
+
+            Returns:
+                A tuple of column definition and data.
+            """
+            schema = [('Month', 'string'), ('KLOCC/Hour', 'number')]
+            data = []
+            valid_records = self._get_valid_records()
+            product_records = ProductFilter(valid_records, product).filter()
+            stat = RecordsStatistics(product_records)
+            stat_by_month = {}
+            for month, group in stat.groupby_review_creation_month:
+                _stat = RecordsStatistics(list(group))
+                stat_by_month[month] = _stat.inspection_rate
+            month_range = utils.month_range(self._start_date, self._end_date)
+            for month in month_range:
+                data.append([month, stat_by_month.get(month, 0)])
+            self._save(setting, schema, data)
+
+        # Start to process for all products.
         for product, setting in self._settings[name].items():
-            self._process_inspection_rate_by_month_from_product(
+            _process_inspection_rate_by_month_from_product(
                 product, setting)
